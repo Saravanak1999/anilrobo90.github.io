43981931cc13361ae815aa2f1bddc41f599dfff7
Anil Nair <anilcoll90@gmail.com>
2018-10-12 12:52:08 +0530

    Redesigned the whole website using skeleton framework

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
                                                     Robo Bionics Website
                                                     --------------------
 
-Welcome to Robo Bionics, A team of Engineers actively working on Upper Limb Prothesis. You Can reach us at contact@robobionics.in.
+Welcome to Robo Bionics, A team of Engineers actively working on Upper Limb Prothesis. 
+We redesigned the whole website using skeleton framework
+You Can reach us at contact@robobionics.in.
---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+  // Menu initialization
+
+  var $menuIcon = $('.menu-icon'),
+      $navigation = $('.navigation'),
+      $mainNavigation = $('.main-navigation'),
+      $navigationLink = $('.main-navigation a');
+
+  $(window).scroll(function() {
+    if(window.scrollY > window.outerHeight) {
+      $menuIcon.addClass('active');
+    } else {
+      $menuIcon.removeClass('active');
+    }
+  });
+
+  $menuIcon.click(function(e) {
+    e.preventDefault();
+
+    $navigation.toggleClass('active');
+  });
+
+  $mainNavigation.singlePageNav({
+      filter: ':not(.external)',
+      speed: 1000,
+      currentClass: 'current',
+      easing: 'swing',
+      updateHash: false,
+      beforeStart: function() {
+      },
+      onComplete: function() {
+        $navigation.removeClass('active');
+      }
+  });
+
+  // Scrollreveal initialize
+
+  var config = {
+
+    easing: 'hustle',
+    reset:  false,
+    delay:  'onload',
+    opacity: .2,
+    vFactor: 0.2,
+    mobile: false
+  }
+
+  window.sr = new scrollReveal( config );
+});
